1d1ec04d936c7f6f3e9c5573b1855be07e9d361f
awangran <ashleero.yin@gmail.com>
2025-02-15 17:57:55 -0500

    ingresadas funciones array y linked

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,2 +1,22 @@
 def new_queue():
-    pass
+    new_queue = {
+        'size': 0,
+        'elements': []
+    }
+    return new_queue
+
+def enqueue(my_queue, element):
+    last = my_queue['size']-1
+    my_queue['elements'][last] = element
+    my_queue['size'] += 1
+    return my_queue
+        
+
+def size(my_queue):
+    return my_queue['size']
+
+def dequeue(my_queue):
+    my_queue['elements'][0] = element
+    
+    my_queue['size'] -= 1
+    return my_queue
